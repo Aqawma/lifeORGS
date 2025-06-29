cfe687f63341f79692bd3bd48969d954bfe67cb7
Max <mgoodfellow@middlebury.edu>
2025-06-28 22:51:24 -0400

    Add `tokenModify` class for modifying events and tasks in the database
    - Implements methods `modifyEvent` and `modifyTask` for updating database records.
    - Integrates `TokenRemove` to handle event removal during task modification.
    
    Signed-off-by: Max <mgoodfellow@middlebury.edu>

---
+++ parsing/tokenModify.py
@@ -0,0 +1,26 @@
+from parsing.tokenize import Tokens
+from utils.dbUtils import ConnectDB
+from parsing.tokenRemove import TokenRemove
+
+class tokenModify:
+    def __init__(self, tokenObject: Tokens):
+        self.tokens = tokenObject
+
+    def modifyEvent(self):
+        connector = ConnectDB()
+        connector.conn.execute("UPDATE events SET ?=? WHERE event=?", (self.tokens.modVerb,
+                                                                       self.tokens.modContext,
+                                                                       self.tokens.iD))
+        connector.conn.commit()
+
+    def modifyTask(self):
+        connector = ConnectDB()
+        connector.conn.execute("SELECT event FROM events WHERE event=?", (self.tokens.iD,))
+        row = connector.c.fetchone()
+
+        if len(row) != 0:
+
+            TokenRemove.removeEvent(TokenRemove(self.tokens))
+            connector.conn.execute("UPDATE tasks SET ?=? WHERE task=?", (self.tokens.modVerb,
+                                                                         self.tokens. modContext,
+                                                                         self.tokens.iD))
